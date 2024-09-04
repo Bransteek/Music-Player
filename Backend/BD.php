9886a37cf49bf53f60a4d5e670e6aa864f537ff4
brianscv1 <162756006+brianscv1@users.noreply.github.com>
2024-09-04 11:02:01 -0500

    oee

--- Backend/BD.php
+++ Backend/BD.php
@@ -1,13 +1,13 @@
 <?php
 class conexion{
-    function conexion_bd(){
+    public static function conexion_bd(){
         $host ="localhost";
         $dbname="50_de_cilantro";
         $username="postgres";
         $password="1040871723";
 
         try{
-            $conn = new PDO ("pgsql:host = $host; dbname = $dbname;$username,$password");
+            $conn = new PDO ("pgsql:host = $host; dbname = $dbname",$username,$password);
             echo "Se conecto correctamente a la base de datos";
 
         }catch(PDOException $exp){
