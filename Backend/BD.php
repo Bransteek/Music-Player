9d1de2aa8de234af7fcfae93b78ec4d519934477
brianscv1 <162756006+brianscv1@users.noreply.github.com>
2024-10-22 15:22:22 -0500

    :)

--- Backend/BD.php
+++ Backend/BD.php
@@ -10,7 +10,7 @@ class conexion
 
         try {
             $conn = new PDO("pgsql:host = $host; dbname = $dbname", $username, $password);
-            echo "Se conecto correctamente a la base de datos";
+            
 
         } catch (PDOException $exp) {
             echo ("No se pudo conectar a la base de datos, $exp");
